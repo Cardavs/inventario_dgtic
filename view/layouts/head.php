5c0e4aae6a340d1c8f42cfee17733192f6ae9531
Roan <argonza_roan@outlook.com>
2023-08-08 15:35:44 -0600

    bootsrap agregado

--- view/layouts/head.php
+++ view/layouts/head.php
@@ -3,12 +3,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Inventario</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/inventario_dgtic/view/main.css">
     <link rel="stylesheet" href="/inventario_dgtic/view/css/normalize.css">
     <link rel="stylesheet" href="/inventario_dgtic/view/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="/inventario_dgtic/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
