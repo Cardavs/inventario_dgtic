2e2797634b8580402cf259dc7c0364f85511a31b
Cardavs <josuecardav@gmail.com>
2023-12-07 11:00:26 -0600

    corrección Css

--- index.php
+++ index.php
@@ -15,29 +15,25 @@ include(VALIDATION_PHP . '/validate-login.php');
 
 <body>
     <?php include(LAYOUT . '/header.php'); ?>
-
-    <h2 class="titulo mt-5 mb-5">Inicio de Sesión</h2>
-
-    <form class="container col-md-12 col-sm-4 formulario needs-validation" method="post" novalidate>
-        <div class="col mb-5">
-            <label for="correo">Ingrese su Correo electrónico</label>
-            <input id="correo" name="correo" class="form-control form-control-lg" type="email" placeholder="Correo Electrónico" required>
-            <div class="invalid-feedback">
-                No es un correo electrónico válido o es necesario colocarlo.
+        <form class="container col-sm-4 formulario needs-validation mt-5" method="post" novalidate>
+            <div class="col mb-5">
+                <label for="correo">Ingrese su Correo electrónico</label>
+                <input id="correo" name="correo" class="form-control form-control-lg" type="email" placeholder="Correo Electrónico" required>
+                <div class="invalid-feedback">
+                    No es un correo electrónico válido o es necesario colocarlo.
+                </div>
             </div>
-        </div>
-        <div class="col mb-5">
-            <label for="password">Ingrese su Contraseña</label>
-            <input name="password" class="form-control form-control-lg" type="password" placeholder="Contraseña" required>
-            <div class="invalid-feedback">
-                Es necesario colocar su Contraseña.
+            <div class="col mb-5">
+                <label for="password">Ingrese su Contraseña</label>
+                <input name="password" class="form-control form-control-lg" type="password" placeholder="Contraseña" required>
+                <div class="invalid-feedback">
+                    Es necesario colocar su Contraseña.
+                </div>
             </div>
-        </div>
-        <div class="row justify-content-sm-center btn-pass">
-            <button name="login" class="btn btn-primary botonCreateuser" type="submit" value="login">Iniciar Sesión</button>
-        </div>
-    </form>
-    </div>
+            <div class="row justify-content-sm-center btn-pass">
+                <button name="login" class="btn btn-primary botonCreateuser" type="submit" value="login">Iniciar Sesión</button>
+            </div>
+        </form>
     <script src="/inventario_dgtic/controllers/validation/js/form-validation-empty.js"></script>
     <?php include(LAYOUT . '/footer.php'); ?>
 </body>
